8b1ef51c8617366faaa3249bce42914138e6f256
lpdumas <louis.philippe.dumas@gmail.com>
2012-12-17 10:18:44 -0500

    fix - Guardfile

--- web/assets/config.rb
+++ web/assets/config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/assets/"
 css_dir = "stylesheets"
-sass_dir = "stylesheets/scss"
+sass_dir = "stylesheets"
 images_dir = "images"
 javascripts_dir = "javascripts"
 

873679cbcc762ba2e4a1c143f2e1c933c600eb96
lpdumas <louis.philippe.dumas@gmail.com>
2012-12-18 11:30:35 -0500

    speaker images

--- web/app.php
+++ web/app.php
@@ -146,7 +146,7 @@ $app->get('/', $index)
 
 $app->get('/horaire/{day}/{slug}-{id}', function ($day = null, $slug = null, $id = null) use ($app) {
     $sessionId = (int) $id;
-    $sql = "SELECT session.*, speaker.name AS speaker_name, room.name AS room_name FROM session JOIN speaker ON speaker.id = session.speaker_id JOIN room ON room.id = session.room_id WHERE session.id = ?";
+    $sql = "SELECT session.*, speaker.name AS speaker_name, speaker.bio AS speaker_bio, speaker.image AS speaker_image, speaker.title AS speaker_title, speaker.entreprise AS speaker_entreprise, speaker.website AS speaker_website, speaker.twitter AS speaker_twitter, room.name AS room_name FROM session JOIN speaker ON speaker.id = session.speaker_id JOIN room ON room.id = session.room_id WHERE session.id = ?";
     $session = $app['db']->fetchAssoc($sql, array($sessionId));
 
     if ($session) {
